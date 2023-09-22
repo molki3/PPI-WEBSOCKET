d6c8b14e2ab88b111221d47334ea56674d3eb317
Manuel Navarro <molman3@hotmail.com>
2023-09-21 23:26:14 -0600

    Added a Procfile.

---
+++ README.md
@@ -0,0 +1,7 @@
+# WEB SOCKETS
+-Communication between server and client (browser).
+-Bidirectional.
+-Allows real-time data flow.
+
+# START PROJECT
+npm init
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//app setup
+var app = express();
+
+//server
+var server = app.listen(3000, function(){
+    console.log('listen to req  on port 3000');
+});
+
+//static files
+app.use(express.static('public'));
+
+//socket setup
+var io = socket(server);
+
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
